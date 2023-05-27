d4fa0a40b34d27c4ada009a8db368ecaec92cc38
Данил Мелихов <melihovdanil8@gmail.com>
2023-05-27 15:30:01 +0600

    SignUp bloc created

---
+++ lib/pages/sign_up/bloc/sign_up_events.dart
@@ -0,0 +1,23 @@
+abstract class SignUpEvent {
+  const SignUpEvent();
+}
+
+class UserNameEvent extends SignUpEvent {
+  final String userName;
+  const UserNameEvent(this.userName);
+}
+
+class EmailEvent extends SignUpEvent {
+  final String email;
+  const EmailEvent(this.email);
+}
+
+class PasswordEvent extends SignUpEvent {
+  final String password;
+  const PasswordEvent(this.password);
+}
+
+class RePasswordEvent extends SignUpEvent {
+  final String rePassword;
+  const RePasswordEvent(this.rePassword);
+}
